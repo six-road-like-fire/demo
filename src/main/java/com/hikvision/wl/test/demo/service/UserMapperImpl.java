3e454b2947c0054e35d3ebd76c8eb40132291bcd
six-road-like fire <1126979165@qq.com>
2019-08-06 19:33:56 +0800

    第一次修改

---
+++ src/main/java/com/hikvision/wl/test/demo/service/UserMapperImpl.java
@@ -0,0 +1,39 @@
+package com.hikvision.wl.test.demo.service;
+
+import com.hikvision.wl.test.demo.mapper.UserMapper;
+import com.hikvision.wl.test.demo.model.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserMapperImpl implements UserMapper {
+
+    @Autowired
+    private UserMapper userMapper;
+    @Override
+    public List<UserEntity> getAll(){
+        return  userMapper.getAll();
+    }
+
+    @Override
+    public  UserEntity getOne(Long id){
+        return userMapper.getOne(id);
+    }
+
+    @Override
+    public  void insert(UserEntity user){
+        userMapper.insert(user);
+    }
+
+    @Override
+    public void update(UserEntity user){
+        userMapper.update(user);
+    }
+
+    @Override
+    public   void delete(Long id){
+        userMapper.delete(id);
+    }
+}

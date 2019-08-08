b40b806f858a566628fe9954fbb6099e90631d62
six-road-like fire <1126979165@qq.com>
2019-08-08 11:29:28 +0800

    单个文件测试

--- src/main/java/com/hikvision/wl/test/demo/dao/LoginDao.java
+++ src/main/java/com/hikvision/wl/test/demo/dao/LoginDao.java
@@ -2,7 +2,7 @@ package com.hikvision.wl.test.demo.dao;
 
 import com.hikvision.wl.test.demo.model.UserEntity;
 
-
+/*just a test */
 
 public interface LoginDao {
     UserEntity login(String username, String password);
